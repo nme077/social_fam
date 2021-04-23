d69a7869a331145725bb596a1be1d68e38ca98fb
Nicholas Eveland <nmemusic77@gmail.com>
2021-04-23 13:54:35 -0600

    Update email

--- routes/settings.js
+++ routes/settings.js
@@ -149,8 +149,8 @@ setInterval(keepAwake, 5000);
 function keepAwake() {
     return keepAwakeNum += 1;
 };
-//sendTestEmail();
-//setInterval(sendTestEmail, 43200000); // Send test email every 12 hours to keep credentials active
+sendTestEmail();
+setInterval(sendTestEmail, 43200000); // Send test email every 12 hours to keep credentials active
 // Send email to check status of gmail server
 function sendTestEmail() {
     async.waterfall([
