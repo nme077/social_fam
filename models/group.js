cc5684c2a157bd3679dd56481937b7945acff658
Nicholas Eveland <nmemusic77@gmail.com>
2021-04-28 20:04:18 -0600

    Fix bug with 'group' model

--- models/group.js
+++ models/group.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose');
 const groupSchema = new mongoose.Schema({
 	name: {type: String, unique: true, required: true},
     adminUser: {
-        type: mongoose.Schema.Types.ObjectId,
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
             ref: 'User'
+        },
+        username: String
     },
     users: [
 		{
